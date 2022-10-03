99735fd72e91c06cd10b9fe4bb37c4f8d6bac20d
puter2712git <puter2712goo@gmail.com>
2022-10-03 21:34:30 +0900

    11022 A+B - 8

---
+++ Baekjoon/11022.py
@@ -0,0 +1,9 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input().rstrip())
+
+for i in range(t):
+  a, b = map(int, input().rstrip().split())
+
+  print('Case #{0}: {1} + {2} = {3}'.format(str(i + 1), str(a), str(b), str(a + b)))
