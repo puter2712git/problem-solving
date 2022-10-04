a9b60de4320b42ff32b194c517c81b5859198650
puter2712git <puter2712goo@gmail.com>
2022-10-04 17:40:30 +0900

    2480 주사위 세개

---
+++ Baekjoon/2480.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+
+dice = list(map(int, input().rstrip().split()))
+dice.sort()
+
+if (dice[0] == dice[1] and dice[0] == dice[2]):
+  print(10000 + dice[0] * 1000)
+elif (dice[0] == dice[1] or dice[1] == dice[2]):
+  print(1000 + dice[1] * 100)
+else:
+  print(dice[2] * 100)
