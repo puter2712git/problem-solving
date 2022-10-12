32ee02bd45f5c13b8a0af47367fd7cbf51d6ae73
puter2712git <puter2712goo@gmail.com>
2022-10-12 15:17:35 +0900

    5086 배수와 약수

---
+++ Baekjoon/5086.py
@@ -0,0 +1,14 @@
+import sys
+input = sys.stdin.readline
+
+while True:
+  a, b = map(int, input().rstrip().split())
+  if (a == 0 and b == 0):
+    break
+  
+  if    a % b == 0:
+    print('multiple')
+  elif  b % a == 0:
+    print('factor')
+  else:
+    print('neither')
