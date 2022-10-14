2ee157d16bc721231b11877b95e53fa3c2ae6fd8
puter2712git <puter2712goo@gmail.com>
2022-10-15 03:04:31 +0900

    10987 모음의 개수

---
+++ Baekjoon/10987.py
@@ -0,0 +1,11 @@
+import sys
+input = sys.stdin.readline
+
+word = str(input().rstrip())
+count = 0
+
+for char in word:
+  if char == 'a' or char == 'e' or char == 'i' or char == 'o' or char == 'u':
+    count += 1
+    
+print(count)
