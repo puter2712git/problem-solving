ea5ef78ceaf6d48c961a3df06f842cc960c72e08
puter2712git <puter2712goo@gmail.com>
2022-11-14 05:09:05 +0900

    11052 카드 구매하기

---
+++ Baekjoon/11052.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+cards = [ None ]
+cards += list(map(int, input().rstrip().split()))
+dp_cards = [ None ] * (n + 1)
+dp_cards[1] = cards[1]
+
+for i in range(2, n + 1):
+  dp_cards[i] = cards[i]
+  for j in range(1, i):
+    dp_cards[i] = max(dp_cards[i], dp_cards[i - j] + cards[i - (i - j)])
+
+print(dp_cards[n])
