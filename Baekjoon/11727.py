b3985b9686a7e6a22223fdadd1ba3dec932789ed
puter2712git <puter2712goo@gmail.com>
2022-11-13 19:35:12 +0900

    11727 2×n 타일링 2

---
+++ Baekjoon/11727.py
@@ -0,0 +1,16 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+dp_ways = [ None ] * (n + 1)
+dp_ways[0] = -1
+dp_ways[1] = 1
+
+for i in range(2, n + 1):
+  if i == 2:
+    dp_ways[i] = 3
+    continue
+  dp_ways[i] = dp_ways[i - 1] + dp_ways[i - 2] * 2
+
+print(dp_ways[n] % 10007)
