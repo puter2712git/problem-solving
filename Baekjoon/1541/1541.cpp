63cb2064d78254e437f119af5a7360ffe2e8eda8
puter2712git <puter2712goo@gmail.com>
2023-01-12 07:08:20 +0900

    1541 잃어버린 괄호

---
+++ Baekjoon/1541/1541.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+using namespace std;
+
+
+void parse_expression(string _target, vector<string> &_parsed_str)
+{
+  int len = _target.length();
+  int start_index = 0;
+
+  for (int i = 0; i < len; i++) {
+    if (!isdigit(_target[i])) {
+      _parsed_str.push_back(_target.substr(start_index, i - start_index));
+      _parsed_str.push_back(_target.substr(i, 1));
+      start_index = i + 1;
+    }
+  }
+  _parsed_str.push_back(_target.substr(start_index, len - start_index));
+}
+
+
+int get_min_value(vector<string> &_parsed_str)
+{
+  int ret = 0;
+  bool is_minus = false;
+
+  for (int i = 0; i < _parsed_str.size(); i++) {
+    if (atoi(_parsed_str[i].c_str())) {
+      if (is_minus) {
+        ret -= stoi(_parsed_str[i]);
+      } else {
+        ret += stoi(_parsed_str[i]);
+      }
+    }
+
+    if (_parsed_str[i] == "-") {
+      is_minus = true;
+    }
+  }
+
+  return ret;
+}
+
+
+int main()
+{
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  string expression;
+  cin >> expression;
+
+  vector<string> parsed_str;
+  parse_expression(expression, parsed_str);
+
+  cout << get_min_value(parsed_str) << '\n';
+
+  return 0;
+}
