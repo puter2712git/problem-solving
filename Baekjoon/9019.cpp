32a218861d4668bf45ced4d6c9b0ebb195760055
Dongjin Kuk <puter2712goo@gmail.com>
2023-05-04 18:20:25 +0900

    9019 DSLR (not solved)

---
+++ Baekjoon/9019.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <queue>
+#include <string>
+#include <algorithm>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+int T;
+int A, B;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    cin >> T;
+
+    for (int i = 0; i < T; i++) {
+        cin >> A >> B;
+    }
+}
