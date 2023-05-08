60e13edd800a95a2f16a6aa371f098f9766cbf7f
Dongjin Kuk <puter2712goo@gmail.com>
2023-05-08 14:49:33 +0900

    11279 최대 힙

---
+++ Baekjoon/11279.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    priority_queue<int> pq;
+    int op_count;
+
+    cin >> op_count;
+
+    while (op_count--) {
+        int n;
+        cin >> n;
+
+        if (n == 0) {
+            if (pq.empty()) {
+                cout << 0 << '\n';
+                continue;
+            }
+
+            cout << pq.top() << '\n';
+            pq.pop();
+        }
+        else {
+            pq.push(n);
+        }
+    }
+
+    return 0;
+}
