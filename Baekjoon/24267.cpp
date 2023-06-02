6758e1726f0844e6d0b973b723a7f96bc4188ce1
Dongjin Kuk <puter2712goo@gmail.com>
2023-06-02 10:26:23 +0900

    24267 알고리즘 수업 - 알고리즘의 수행 시간 6

---
+++ Baekjoon/24267.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    long long n;
+
+    cin >> n;
+
+    cout << (n * (n - 1) * (n - 2)) / 6 << "\n";
+    cout << 3 << "\n";
+
+    return 0;
+}
