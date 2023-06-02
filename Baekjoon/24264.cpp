edb16a4d7de400b9ae4d60b3177c6531f853ca4b
Dongjin Kuk <puter2712goo@gmail.com>
2023-06-02 10:10:22 +0900

    24264 알고리즘 수업 - 알고리즘의 수행 시간 3

---
+++ Baekjoon/24264.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    long long n;
+    
+    cin >> n;
+
+    cout << n * n << "\n";
+    cout << 2 << "\n";
+
+    return 0;
+}
