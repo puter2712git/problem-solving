794b5b0514c2e0487489c75a7ec9168a04b0d57b
Dongjin Kuk <85764911+puter2712git@users.noreply.github.com>
2024-03-19 21:57:47 +0900

    15688

---
+++ Baekjoon/15000~15999/15688.cpp
@@ -0,0 +1,30 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int numCount;
+    vector<int> numbers;
+
+    cin >> numCount;
+    for (int i = 0; i < numCount; i++) {
+        int tmp;
+        cin >> tmp;
+        numbers.push_back(tmp);
+    }
+
+    sort(numbers.begin(), numbers.end(), less<int>());
+
+    for (int i = 0; i < numCount; i++) {
+        cout << numbers[i] << "\n";
+    }
+
+    return 0;
+}
