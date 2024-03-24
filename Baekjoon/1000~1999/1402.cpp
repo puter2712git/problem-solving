051d9b13e9e138d8695a4dd9ec2916c5bd828232
Dongjin Kuk <85764911+puter2712git@users.noreply.github.com>
2024-03-24 17:00:04 +0900

    1402

---
+++ Baekjoon/1000~1999/1402.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    int testcase;
+    cin >> testcase;
+
+    for (int i = 0; i < testcase; i++) {
+        int a, b;
+        cin >> a >> b;
+        cout << "yes"
+             << "\n";
+    }
+
+    return 0;
+}
