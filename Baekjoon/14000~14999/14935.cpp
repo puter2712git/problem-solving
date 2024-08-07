71bfd4d1e7118eea1e7215edb526dc8916603b72
Dongjin Kuk <85764911+puter2712git@users.noreply.github.com>
2024-08-07 20:59:56 +0900

    14935

---
+++ Baekjoon/14000~14999/14935.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int x;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+
+	cin >> x;
+	cout << "FA\n";
+
+	return 0;
+}
