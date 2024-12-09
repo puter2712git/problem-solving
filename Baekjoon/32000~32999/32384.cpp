32c249e110ad5a01c7f17e790ca1f175257c5761
puter2712git <puter2712goo@gmail.com>
2024-12-10 00:17:42 +0900

    32384

---
+++ Baekjoon/32000~32999/32384.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cin >> N;
+
+    for (int i = 0; i < N; i++)
+    {
+        cout << "LoveisKoreaUniversity ";
+    }
+    cout << "\n";
+
+    return 0;
+}
