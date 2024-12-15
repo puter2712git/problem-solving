9bfa7e36d865bc0546db697ede1bfc91b3ea0e71
puter2712git <puter2712goo@gmail.com>
2024-12-15 14:41:54 +0900

    2393

---
+++ Baekjoon/2000~2999/2393.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+
+    cout << "  ___  ___  ___\n";
+    cout << "  | |__| |__| |\n";
+    cout << "  |           |\n";
+    cout << "   \\_________/\n";
+    cout << "    \\_______/\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |     |\n";
+    cout << "     |_____|\n";
+    cout << "  __/       \\__\n";
+    cout << " /             \\\n";
+    cout << "/_______________\\\n";
+
+    return 0;
+}
